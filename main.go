5acbcdbe13a3e1351798a81aa87c31ed8cf64faf
based <thebasedcoder@proton.me>
2025-05-15 12:47:59 +0330

    add peer

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net"
+import (
+	"log"
+	"log/slog"
+	"net"
+)
 
 const defaultListenAddr = ":5001"
 
@@ -10,7 +14,10 @@ type Config struct {
 
 type Server struct {
 	Config
-	ln net.Listener
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
 }
 
 func (s *Server) Start() error {
@@ -19,7 +26,41 @@ func (s *Server) Start() error {
 		return err
 	}
 	s.ln = ln
-	return nil
+
+	go s.loop()
+
+	slog.Info("Server running...", "listen address", s.ListenAddr)
+	return s.accepLoop()
+
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+
+		}
+	}
+}
+
+func (s *Server) accepLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "error", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn)
+	s.addPeerCh <- peer
+	peer.readLoop()
 }
 
 func NewServer(cfg Config) *Server {
@@ -27,8 +68,14 @@ func NewServer(cfg Config) *Server {
 		cfg.ListenAddr = defaultListenAddr
 	}
 	return &Server{
-		Config: cfg,
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
 	}
 }
 
-func main() {}
+func main() {
+	server := NewServer(Config{})
+	log.Fatal(server.Start())
+}
